3692924f0956e3ecf3b3a5ed824dc5fad2398820
JCMS22 <jeanlavoie22@gmail.com>
2017-08-20 17:28:29 -0400

    gulp 2

--- conFusion/gulpfile.js
+++ conFusion/gulpfile.js
@@ -5,7 +5,15 @@
 
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    browserSync = require('browser-sync');
+    browserSync = require('browser-sync'),
+    del = require('del'),
+    uglify = require('gulp-uglify'),
+    usemin = require('gulp-usemin'),
+    rev = require('gulp-rev'),
+    cleanCss = require('gulp-clean-css'),
+    flatmap = require('gulp-flatmap'),
+    htmlmin = require('gulp-htmlmin'),
+    imagemin = require('gulp-imagemin');
 
 
 gulp.task('sass', function () {
@@ -34,7 +42,43 @@ gulp.task('browser-sync', function () {
 
 });
 
+// Clean
+gulp.task('clean', function() {
+    return del(['dist']);
+});
+
+gulp.task('copyfonts', function() {
+    gulp.src('./node_modules/font-awesome/fonts/**/*.{ttf,woff,eof,svg}*')
+        .pipe(gulp.dest('./dist/fonts'));
+});
+
 // Default task
 gulp.task('default', ['browser-sync'], function() {
     gulp.start('sass:watch');
+});
+
+// Images
+gulp.task('imagemin', function() {
+    return gulp.src('img/*.{png,jpg,gif}')
+        .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+        .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('usemin', function() {
+    return gulp.src('./*.html')
+        .pipe(flatmap(function(stream, file){
+            return stream
+                .pipe(usemin({
+                    css: [ rev() ],
+                    html: [ function() { return htmlmin({ collapseWhitespace: true })} ],
+                    js: [ uglify(), rev() ],
+                    inlinejs: [ uglify() ],
+                    inlinecss: [ cleanCss(), 'concat' ]
+                }))
+        }))
+        .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('build',['clean'], function() {
+    gulp.start('copyfonts','imagemin','usemin');
 });
